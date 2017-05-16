4382b25312d2690c3cf45456d82ba64e0a977bb9
DESKTOP-ELVJ4VF\Elise <elise@apelab.ch>
2017-05-16 16:45:40 +0200

    DemoScene (Dependence)

---
+++ SpatialStories_SDK/Assets/DemoScenes/Scripts/Gaze_TriggerPlatform.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Gaze
+{
+    public class Gaze_TriggerPlatform : Gaze_AbstractBehaviour
+    {
+
+        public Transform objectToParent;
+        public bool parented = true;
+
+
+
+
+        protected override void OnActive()
+        {
+        }
+
+        protected override void OnAfter()
+        {
+        }
+
+        protected override void OnBefore()
+        {
+        }
+
+        protected override void OnReload()
+        {
+        }
+
+        protected override void OnTrigger()
+        {
+
+            if (parented)
+            {
+                objectToParent.parent = gazable.Root.transform;
+            }
+            else
+            {
+
+                objectToParent.parent = null;
+
+            }
+        }
+    }
+}
+
