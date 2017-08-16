dac2b86e96d83da285314f76b32e5ecd9ab6d63e
DESKTOP-ELVJ4VF\Elise <arthur34000@hotmail.fr>
2017-08-16 09:53:07 +0200

    Fixed Vive positioning script on camera

--- SpatialStories_SDK/Assets/DemoScenes/Assets/Scripts/Gaze_VivePositioning.cs
+++ SpatialStories_SDK/Assets/DemoScenes/Assets/Scripts/Gaze_VivePositioning.cs
@@ -13,10 +13,11 @@ public class Gaze_VivePositioning : MonoBehaviour
     void OnControlerSetup(Gaze_Controllers _type)
     {
         if (_type == Gaze_Controllers.HTC_VIVE)
+        {
             transform.position = new Vector3(transform.position.x, viveHeigth, transform.position.z);
-
+            Gaze_Teleporter.playerHeightBeforeTeleport = viveHeigth;
+        }
         Gaze_InputManager.OnControlerSetup -= OnControlerSetup;
-        Gaze_Teleporter.playerHeightBeforeTeleport = viveHeigth;
         Destroy(this);
     }
 }
