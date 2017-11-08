81bea322a7c4eb4829d631923e187a1893380c7b
mike <mike@apelab.ch>
2017-11-08 17:22:04 +0100

    Watson

---
+++ SpatialStories_SDK/Assets/SpatialStories_Modules/Watson/SpatialStories/CustomConditions/CC_WatsonWordRecognized.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+namespace Gaze
+{
+    public class CC_WatsonWordRecognized : Gaze_AbstractConditions
+    {
+        public string WordToRecognize;
+        
+        private void Start()
+        {
+            FindObjectOfType<VoiceSpawner>().AddWord(WordToRecognize, Validate);
+        }
+
+        public void Validate()
+        {
+            ValidateCustomCondition(true);
+        }
+    }
+}
