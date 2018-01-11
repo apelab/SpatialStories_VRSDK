f753e0236d33c15ad67989424c514db7e940c61d
DESKTOP-ELVJ4VF\Elise <arthur34000@hotmail.fr>
2018-01-11 11:49:34 +0100

    ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- SpatialStories_SDK/Assets/SpatialStories/Core/SpatialStories_Version.cs
+++ SpatialStories_SDK/Assets/SpatialStories/Core/SpatialStories_Version.cs
@@ -1,9 +1,7 @@
-﻿using System;
-
-namespace Gaze
+﻿namespace Gaze
 {
     public class SpatialStories_Version
     {
-        public const string VERSION = "V.0.2b6";
+        public const string VERSION = "V.0.3b5";
     }
 }
